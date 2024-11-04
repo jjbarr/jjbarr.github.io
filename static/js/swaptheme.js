4dec58c87932ea1d9fd84859f46ac5c0b0acd794
Joshua Barrett <jjbarr.public@gmail.com>
2024-11-04 12:13:54 -0500

    improved JS greatly

--- static/js/swaptheme.js
+++ static/js/swaptheme.js
@@ -1,35 +1,6 @@
 "use strict";
-const THEME = "sel";
-const LIGHT = "w";
-const DARK = "b";
-const ICON = {
-    "w": "moon",
-    "b": "sun"
-};
-const COLORS = [
-    "bg0",
-    "bg1",
-    "bg2",
-    "dim0",
-    "fg0",
-    "fg1",
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "cyan",
-    "magenta",
-    "violet",
-    "br-red",
-    "br-orange",
-    "br-yellow",
-    "br-green",
-    "br-blue",
-    "br-cyan",
-    "br-magenta",
-    "br-violet",
-];
+const SCHEMES = ["light", "dark"];
+const ICON = ["moon", "sun"];
 
 function update_scheme(curscheme, container) {
     //yes, this the Wrong Way to this, but the Right Way is... painful.
@@ -38,28 +9,26 @@ function update_scheme(curscheme, container) {
   <title>Switch Color Scheme</title>
   <use xlink:href="/fonticons/feather.svg#${ICON[curscheme]}"/>
 </svg>`;
-    for(const color of COLORS) {
-        document.documentElement.style
-            .setProperty(`--${color}`,
-                         `var(--${THEME}-${curscheme}-${color})`);
-    }
+    document.body.setAttribute('data-colorscheme', curscheme);
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
-    let curscheme = window.localStorage.getItem("prefcolors")
-        || (window.matchMedia("(prefers-color-scheme: dark)").matches
-            ? DARK
-            : LIGHT);
-    if (curscheme !== DARK && curscheme !== LIGHT) {
+    let curscheme = document.cookie.split('; ')
+        .map(cs => cs.split("="))
+        .filter(c => c[0] == "colorscheme")
+        .map(c => c[1]).at(0);
+    if (curscheme === undefined
+        || parseInt(curscheme, 10) === NaN
+        || curscheme >= SCHEMES.length) {
         curscheme =
-            (window.matchMedia("(prefers-color-scheme: dark)").matches
-             ? DARK : LIGHT);
+            (window.matchMedia("(prefers-color-scheme: dark)").matches) + 0;
     }
     let container = document.getElementById("change-color-scheme");
     update_scheme(curscheme, container);
     container.addEventListener("click", (e) => {
-        curscheme = curscheme === DARK? LIGHT : DARK;
-        window.localStorage.setItem("prefcolors", curscheme);
+        curscheme = (curscheme + 1) % SCHEMES.length;
+        document.cookie =
+            `colorscheme=${curscheme}; expires=${(new Date(Date.now()+ 86400*365*1000)).toUTCString()}; path=/; SameSite=Lax;`;
         update_scheme(curscheme, container);
     });
 });
