7793898b5f607ff1d9ecbc231f9ab0355e06b520
Joshua Barrett <jjbarr@ptnote.dev>
2025-03-19 09:40:59 -0400

    fixed deploy script for older scp binaries

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf public && zola build -o public && scp -pr public/. web@anubis.bahamut.monster:ptnote.dev && rm -rf public
+rm -rf public && zola build -o public && scp -spr public/. web@anubis.bahamut.monster:ptnote.dev && rm -rf public
